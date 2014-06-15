0e4238a521d6c02238054e0ff4edf76aa6f819b9
Neil Chudleigh <nchudei@uwo.ca>
2014-06-15 08:27:14 -0400

    postgres Server

--- app/foo/views.py
+++ app/foo/views.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import, print_function
 
-from bson import json_util, objectid
 from flask import request, jsonify, send_file
 from . import foo
 from app import db
+from app.models import 
 
 
 @foo.route('/', methods=['GET'])
@@ -16,3 +16,39 @@ def index():
       }
     }), 200
 
+
+@foo.route('/post', methods=["POST"])
+def create_post():
+
+    data = json_util.loads(request.data)
+
+    if not data.get('title'): 
+        return jsonify({
+            'status': 400,
+            'error': 'No title was provided',
+            'result': {
+                'message': 'No title was provided.'
+            }
+        }), 400
+    
+
+    if not data.get('body'): 
+        return jsonify({
+            'status': 400,
+            'error': 'No body was provided',
+            'result': {
+                'message': 'No body was provided.'
+            }
+        }), 400
+   
+
+
+    
+
+    return jsonify({
+        'status': 200,
+        'error': None,
+        'result':{
+            'message':'Post Successful'
+            }
+        }),200
--- run.py
+++ run.py
@@ -5,26 +5,10 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from flask.ext.script import Manager, Server
-
 from app import app
+from flask.ext.script import Manager
 
 manager = Manager(app)
-
-"""Run the server"""
-manager.add_command("dev", Server(
-    use_debugger = True,
-    use_reloader = True,
-    host = '127.0.0.1',
-    port = 5000
-))
-
-manager.add_command("prod", Server(
-    use_debugger = False,
-    use_reloader = False,
-    host = '127.0.0.1',
-    port = 5000
-))
 
 if __name__ == "__main__":
     manager.run()
