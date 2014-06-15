59c706ac5b6ccc4675f3034992b3eb653f2225f1
bbales <ben.bales.dev@gmail.com>
2014-06-15 08:17:02 -0400

    Front end progress

--- app/static/dev/library/app.js
+++ app/static/dev/library/app.js
@@ -1,7 +1,7 @@
 (function(window, document, location, navigator, $, angular, undefined) {
     'use strict';
     
-    angular.module('app', [
+    var app = angular.module('app', [
         'ui.router'
       , 'ui.bootstrap'
       , 'ui.select2'
@@ -13,6 +13,7 @@
     ])
     
     .config(function($stateProvider, $sceProvider, $urlRouterProvider, $logProvider, $translateProvider, growlProvider) {
+        
         $urlRouterProvider.otherwise('home');
         
         $sceProvider.enabled(true);
@@ -20,6 +21,48 @@
         $translateProvider.preferredLanguage('en');
         $logProvider.debugEnabled(false);
         growlProvider.globalTimeToLive(5000);
-    })
-        
-;}(window, document, location, navigator, jQuery, angular, undefined));
+    
+    });
+
+    app.controller("blogController",['$scope',function($scope){
+
+      $scope.resizeContent = function()
+      {
+        var currentWidth = window.innerWidth - $(".drawer").width();
+        $(".content").width(currentWidth);
+      }
+
+      angular.element(window).bind('resize',$scope.resizeContent);
+
+    }]);
+
+    app.directive("card",function(){
+
+        return {
+          restrict : 'E',
+          replace : 'true',
+          templateUrl : 'library/directives/card/card.html',
+          link : function(scope, element)
+          {
+            element.bind('click',function(){
+              $(".card").removeClass("currentCard");
+              element.addClass("currentCard");
+              
+              $(".drawer").animate({scrollTop: ""+(element.offset().top - element.parent().offset().top - element.parent().scrollTop()- $(".drawer").height()/2+ element.height()/2)+"px"},300);
+            });
+          }
+        };
+    });
+    
+    app.directive("blogPost",function(){
+      return {
+        restrict : 'E',
+        replace : 'true',
+        templateUrl : 'library/directives/blogPost/blogPost.html',
+        link : function(scope,element){
+
+        }
+      };
+    });
+
+}(window, document, location, navigator, jQuery, angular, undefined));
---
+++ app/static/dev/styles/scss/config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'app/static/dev/styles'
+sass_dir = 'app/static/dev/styles/scss'
+images_dir = 'app/static/dev/assets/images'
+javascripts_dir = 'app/static/dev/library'
+relative_assets = true
+line_comments = true
