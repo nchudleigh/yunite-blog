886cede84fef82be2167744ee1a90758757e5ddb
bbales <ben.bales.dev@gmail.com>
2014-06-16 21:30:22 -0400

    fugg

--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, print_function
 
-from flask import render_template
+from flask import render_template, send_file
 from . import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return send_file('index.html')
+    return send_file('templates/index.html')
