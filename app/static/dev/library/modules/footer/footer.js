0e44cdb7f7463576bfe59bb4d9f89696aeb1a5b2
bbales <ben.bales.dev@gmail.com>
2014-06-18 23:46:09 -0400

    removed unused modules

--- app/static/dev/library/modules/footer/footer.js
+++
@@ -1,9 +0,0 @@
-(function(window, document, location, navigator, $, angular, undefined) {
-    'use strict';
-
-    angular.module('postster', [
-        'postster.controller'
-      , 'postster.model'
-    ])
-    
-;}(window, document, location, navigator, jQuery, angular, undefined));
--- app/static/dev/library/modules/navigation/navigation.js
+++
@@ -1,9 +0,0 @@
-(function(window, document, location, navigator, $, angular, undefined) {
-    'use strict';
-    
-    angular.module('navigation', [
-        'navigation.model'
-      , 'navigation.controller'
-    ])
-        
-;}(window, document, location, navigator, jQuery, angular, undefined));
